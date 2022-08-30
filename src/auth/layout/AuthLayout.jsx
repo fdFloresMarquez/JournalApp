cabaed64d7fc231ac6f3e08cfadd7e343dd6b91a
Facundo <floresfacundo68@gmail.com>
2022-08-30 12:04:35 -0300

    Styles: fix to Grids

--- src/auth/layout/AuthLayout.jsx
+++ src/auth/layout/AuthLayout.jsx
@@ -5,23 +5,24 @@ export const AuthLayout = ({ children, title = '' }) => {
 
         <Grid
             container
-            spacing={0}            
+            spacing={ 0 }
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
             sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
         >
 
             <Grid 
-                container
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-                className='box-shadow animate__animated animate__fadeIn animate__faster'   
-                xs={3}
-                sx={{
-                    width: { sm: 450, md: 550 },
-                    backgroundColor: 'white',
-                    padding: 3,
-                    borderRadius: 2
-                }}>
+                item
+                className='box-shadow'
+                xs={ 3 }
+                sx={{ 
+                    width: { sm: 450 },
+                    backgroundColor: 'white', 
+                    padding: 3, 
+                    borderRadius: 2 
+                }}
+            >
 
                 <Typography vairant='h5' sx={{ mb: 1 }}>{title}</Typography>
 
